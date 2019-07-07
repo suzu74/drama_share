51d3642ca0e9875c475006eb4d530759636fe6d6
鈴木友章 <tomosuzu74@gmail.com>
2019-07-07 15:18:17 +0900

    Make sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  
-  get '/' ,to: 'top_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/about', to: 'top_pages#about'
-  get '/signup', to: 'users#new'
+  root            to: 'top_pages#home'
+  get '/about',   to: 'top_pages#about'
+  get '/signup',  to: 'users#new'
+  post '/singup', to: 'users#create'
   resources :users
 end
