59faeaec02d2e7c3a5dbc5b98d15ec2b6532bb92
鈴木友章 <tomosuzu74@gmail.com>
2019-07-07 17:30:18 +0900

    Make Login page with login and logout function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root            to: 'top_pages#home'
-  get '/about',   to: 'top_pages#about'
-  get '/signup',  to: 'users#new'
-  post '/singup', to: 'users#create'
+  root               to: 'top_pages#home'
+  get '/about',      to: 'top_pages#about'
+  get '/signup',     to: 'users#new'
+  post '/singup',    to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
