3307f38c86cf09430d2ada603dd1b8388d5e6b6b
鈴木友章 <tomosuzu74@gmail.com>
2019-07-07 12:10:24 +0900

    Make User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  
   get '/' ,to: 'top_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/about', to: 'top_pages#about'
+  get '/signup', to: 'users#new'
 end
