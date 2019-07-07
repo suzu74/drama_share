70193608a59d4c79abedd1f61831514d135308d7
鈴木友章 <tomosuzu74@gmail.com>
2019-07-07 11:43:54 +0900

    Make Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get '/' ,to: 'top_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/about', to: 'top_pages#about'
