0204fe290e809ab73c1a91dcc640ef1c91f4b467
鈴木友章 <tomosuzu74@gmail.com>
2019-07-07 13:37:07 +0900

    partial header

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/about', to: 'top_pages#about'
   get '/signup', to: 'users#new'
+  resources :users
 end
